117e7c6e485fe25d3bc99de588d1b69fbe15da6f
Valdir Júnior <31621712+Valdjr@users.noreply.github.com>
2018-07-01 23:35:01 -0300

    update banco

--- vulcan.sql
+++ vulcan.sql
@@ -1,25 +1,74 @@
-DROP SCHEMA  IF EXISTS vulcan;
-CREATE DATABASE vulcan;
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: 02-Jul-2018 às 04:34
+-- Versão do servidor: 10.1.32-MariaDB
+-- PHP Version: 7.2.5
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `vulcan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `empresa`
+--
 
 CREATE TABLE `empresa` (
   `id` int(11) NOT NULL,
   `nivel` int(1) NOT NULL DEFAULT '0',
   `nome` varchar(100) NOT NULL,
   `moeda` varchar(100) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `empresa`
+--
+
 INSERT INTO `empresa` (`id`, `nivel`, `nome`, `moeda`) VALUES
-(1, 0, 'brasil', 'brl'),
-(2, 0, 'eua', 'usd');
+(1, 0, 'eua', 'usd'),
+(2, 0, 'brasil', 'brl');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `usuario`
+--
 
 CREATE TABLE `usuario` (
   `id` int(11) NOT NULL,
   `idEmpresa` int(11) NOT NULL,
   `usuario` varchar(100) NOT NULL,
   `senha` varchar(100) NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `usuario`
+--
+
 INSERT INTO `usuario` (`id`, `idEmpresa`, `usuario`, `senha`) VALUES
-(1, 1, 'valdir', '123'),
-(2, 2, 'joao', '123');
+(1, 1, 'admin', '123'),
+(2, 2, 'brasil', '123');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `venda`
+--
 
 CREATE TABLE `venda` (
   `id` int(11) NOT NULL,
@@ -29,8 +78,62 @@ CREATE TABLE `venda` (
   `valor` double NOT NULL,
   `cotacao` double NOT NULL,
   `valoruss` double NOT NULL
-);
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `venda`
+--
+
 INSERT INTO `venda` (`id`, `idEmpresa`, `cliente`, `data`, `valor`, `cotacao`, `valoruss`) VALUES
-(1, 1, 'joaozinho da silva', '2018-06-24', 3.78, 3.78, 1),
-(2, 0, 'valdir', '0000-00-00', 358, 3.58, 100),
-(3, 0, 'valdir', '2018-06-26', 358, 3.58, 100);
+(1, 2, 'joaozinho da silva', '2018-06-24', 3.78, 3.78, 1),
+(2, 2, 'valdir', '0000-00-00', 358, 3.58, 100),
+(3, 2, 'valdir', '2018-06-26', 358, 3.58, 100);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `empresa`
+--
+ALTER TABLE `empresa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `venda`
+--
+ALTER TABLE `venda`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `empresa`
+--
+ALTER TABLE `empresa`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `venda`
+--
+ALTER TABLE `venda`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
